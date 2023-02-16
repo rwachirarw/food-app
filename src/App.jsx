caa98c40ba712f68918f659df4e6281a0fccabe1
Edwin Njeru <rwachira.rw@gmail.com>
2023-02-16 11:30:04 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Category from "./components/Category"
+import Food from "./components/Food"
+import HeadlineCards from "./components/HeadlineCards"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+function HeadlineCards() {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+
+      {/* Card */}
+      <div className="relative rounded-xl">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4"> Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">Order Now</button>
+        </div>
+        <img
+        className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2271107/pexels-photo-2271107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+
+      {/* Card */}
+      <div className="relative rounded-xl">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4"> Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">Order Now</button>
+        </div>
+        <img
+        className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2271107/pexels-photo-2271107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>{/* Card */}
+      <div className="relative rounded-xl">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4"> Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">Order Now</button>
+        </div>
+        <img
+        className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2271107/pexels-photo-2271107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+
+    </div>
+  );
+}
+
+export default HeadlineCards;
